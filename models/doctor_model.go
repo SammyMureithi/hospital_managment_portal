d87d0ed3e36fab4c4124974278d4f0d31a7f79b5
kamah-01 <millerbeautyagencies@gmail.com>
2024-06-01 03:02:33 +0300

    doctors model

---
+++ models/doctor_model.go
@@ -0,0 +1,16 @@
+package models
+
+import (
+	"time"
+)
+
+// User struct to define a user model
+type Doctor struct {
+    ID        string    `json:"id"` 
+    FirstName string    `json:"first_name" bson:"first_name" validate:"required,min=3,max=20"`
+    LastName  string    `json:"last_name" bson:"last_name" validate:"required"`
+    Email     string    `json:"email" bson:"email" validate:"required,email"`
+	Phone     string    `json:"phone" bson:"phone" validate:"required,len=10"`
+    CreatedAt time.Time `json:"created_at" bson:"created_at"`
+    UpdatedAt time.Time `json:"updated_at" bson:"updated_at"`
+}
