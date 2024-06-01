b9d958e15a098f3666413f69ab6f0de0ca92502b
kamah-01 <millerbeautyagencies@gmail.com>
2024-06-01 23:11:50 +0300

    admin request

--- requests/doctor_request.go
+++ requests/doctor_request.go
@@ -4,5 +4,5 @@ type DoctorRequest struct {
 	FirstName string `json:"first_name" bson:"first_name" validate:"required,min=3,max=20"`
 	LastName  string `json:"last_name" bson:"last_name" validate:"required,min=3,max=20"`
 	Email     string `json:"email" bson:"email" validate:"required,email"`
-	Phone     string `json:"phone" bson:"phone" validate:"required,len=10"`
+	Phone     string `json:"phone" bson:"phone" validate:"required"`
 }
