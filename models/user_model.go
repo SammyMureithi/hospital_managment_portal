5c1243070e5918b78b15cf8701496f15231a9990
kamah-01 <millerbeautyagencies@gmail.com>
2024-06-03 21:56:24 +0300

    user model

--- models/user_model.go
+++ models/user_model.go
@@ -6,7 +6,7 @@ import (
 
 // User struct to define a user model
 type User struct {
-    ID        string    `json:"id"` // No JSON input expected; remove JSON tag if not serialized
+    ID        string    `json:"id"`
     Username  string    `json:"username" bson:"username" validate:"required,min=3,max=20"`
     Name      string    `json:"name" bson:"name" validate:"required"`
     Email     string    `json:"email" bson:"email" validate:"required,email"`
