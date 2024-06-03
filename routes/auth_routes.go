58d9ea08e8f87404caea9bb6da8581fa15418c6e
kamah-01 <millerbeautyagencies@gmail.com>
2024-06-03 21:57:28 +0300

    auth routes

--- routes/auth_routes.go
+++ routes/auth_routes.go
@@ -8,10 +8,7 @@ import (
 
 // UserRoutes function to initialize user routes
 func UserRoutes(router *mux.Router) {
-      // Apply middleware to the GET routes
-    //   router.Handle("/users", middleware.JWTMiddleware(http.HandlerFunc(controllers.GetUsers))).Methods("GET")
-    //   router.Handle("/user", middleware.JWTMiddleware(http.HandlerFunc(controllers.GetUser))).Methods("GET")
-      
+  
      router.HandleFunc("/auth/register", controllers.SignUp).Methods("POST")
      router.HandleFunc("/auth/login", controllers.Login).Methods("POST")
 }
